7c912dd19f6ae75c99902c914cea79052be48fa0
Siim Tiilen <eritikass@gmail.com>
2021-02-04 21:17:49 +0200

    init add

---
+++ README.md
@@ -0,0 +1,9 @@
+# getting started
+
+1. fork git repository
+1. git clone your fork
+1. install npm packages `npm install`
+1. fix linting errors
+   - `npm run lint` need to be success
+1. create merge(/pull) request from your fork
+   - add your name into merge request description
---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+let url = require("url");
+
+http.createServer(function (req, res) {
+  // send headers
+  res.writeHead(200, {"Content-Type": `text/html`});
+  var q = url.parse(req.url, true).query
+  const txt = q.year + ":" + q.month;res.end(txt);
+}).listen(8080, '0.0.0.0', () => {
+  console.log(`server is up and running, try http://localhost:8080/summer?year=2022&month=2`);
+}); //the server object listens on port 8080
